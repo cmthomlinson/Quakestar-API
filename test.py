bc5ad80009cbe3a31ddbfd6bc9318e8f2eea0018
Charlie <cmthomlinson@gmail.com>
2021-09-06 13:46:31 +1200

    commit

--- test.py
+++ test.py
@@ -84,13 +84,17 @@ def test(floor_id, doc_id):
     info = json.load(f)
     doc = collection.find_one({"_id":ObjectId(doc_id)})
      
-    x3 = doc['26']['x'] * doc['26']['y'] * info[floor_id]['21'][doc['21']]['strength']  
-    x4 = (doc['25']['x'] + doc['25']['y'])*2*info[floor_id]['23'][doc['23']]['strength']  
-    x5 = info[floor_id]['16'][doc['16']]['strength'] 
-    x6 = (doc['24']['x'] + doc['24']['y'])*2*info[floor_id]['13'][doc['13']]['strength']/doc['27']['x']*info[floor_id]['17'][doc['17']]['strength']
+    structure_av = (info[floor_id]['8'][doc['8']]['damage'] + info[floor_id]['9'][doc['9']]['damage'] + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['15'][doc['15']]['damage'] + info[floor_id]['16'][doc['16']]['damage'] + info[floor_id]['17'][doc['17']]['damage'] + info[floor_id]['19'][doc['19']]['damage'] + info[floor_id]['20'][doc['20']]['damage'])/9
+    x8 = doc['38']['x'] * doc['38']['y'] * info[floor_id]['29'][doc['29']]['strength'] + doc['36']['x']
+    x9 = doc['36']['y']*2*(info[floor_id]['32'][doc['31']]['strength'] + info[floor_id]['17'][doc['17']]['strength'])/2
+    x10 = (doc['36']['x'] * doc['36']['y'])*info[floor_id]['18'][doc['18']]['strength'] 
+    x11 = ((doc['36']['x'] + doc['36']['y'])*2)*info[floor_id]['31'][doc['31']]['strength'] 
+    x12 = info[floor_id]['15'][doc['15']]['strength']/2 + doc['36']['x'] * doc['36']['y']*info[floor_id]['16'][doc['16']]['strength'] 
+    x13 = doc['34']['x'] + doc['34']['y']*2*(info[floor_id]['30'][doc['30']]['strength'] + info[floor_id]['13'][doc['13']]['strength'])/2
+    x14 = (doc['34']['x'] * doc['34']['y'])*info[floor_id]['14'][doc['14']]['strength'] + doc['37']['x'] + doc['37']['y']*info[floor_id]['33'][doc['33']]['strength']
+    x15 = info[floor_id]['19'][doc['19']]['strength']/info[floor_id]['28'][doc['28']]['strength'] * doc['42']['y']
     
-    
-    print(x3, x4, x5, x6)
+    print(x8, x9, x10, x11, x12, x13, x14, x15)
 
 
-test("2", "6135539eda861ca6dab49a8f")
+test("3b", "6135715983291b740dc4a71f")
