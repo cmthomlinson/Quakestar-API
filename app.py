b803f61a22eaba0c32e6586819a1d5f3587caba3
Charlie <cmthomlinson@gmail.com>
2021-11-07 19:18:39 +1300

    commit

--- app.py
+++ app.py
@@ -231,14 +231,14 @@ def irregulaties(floor_id, doc):
     info = json.load(f)
     if floor_id == "1":
 
-        return 1
+        return 1*info[floor_id]['22'][doc['22']]['strength']
 
     #2   12-27 Roofcladding-20 Roofarea-25
     if floor_id == "2":
         x0 = 1
         x1 = ((doc['27']['x']*info[floor_id]['17'][doc['17']]['strength'])/(doc['28']['x']*info[floor_id]['19'][doc['19']]['strength']))*1.2
         x2 = ((doc['27']['y']*info[floor_id]['18'][doc['18']]['strength'])/(doc['28']['y']*info[floor_id]['20'][doc['20']]['strength']))*1.2
-        return min(x0, x1, x2)
+        return min(x0, x1, x2)*info[floor_id]['29'][doc['29']]['strength']
 
     #3   12-34 Roofcladding-24 Roofarea-31
     if floor_id == "3":
@@ -247,13 +247,13 @@ def irregulaties(floor_id, doc):
         x2 = ((info[floor_id]['22'][doc['22']]['strength']*doc['34']['y'])/(info[floor_id]['24'][doc['24']]['strength']*doc['35']['y']))*1.2
         x3 = ((info[floor_id]['19'][doc['19']]['strength']*doc['33']['x'])/(info[floor_id]['21'][doc['21']]['strength']*doc['34']['x']))*1.2
         x4 = ((info[floor_id]['20'][doc['20']]['strength']*doc['33']['y'])/(info[floor_id]['22'][doc['22']]['strength']*doc['34']['y']))*1.2
-        return min(x0, x1, x2, x3, x4)
+        return min(x0, x1, x2, x3, x4)*info[floor_id]['36'][doc['36']]['strength']
     #1b   12-27 Roofcladding-20 Roofarea-25
     if floor_id == "1b":
         x0 = 1
         x1 = ((info[floor_id]['18'][doc['18']]['strength']*doc['27']['x'])/(info[floor_id]['16'][doc['16']]['strength']*doc['26']['x']))*1.2
         x2 = ((info[floor_id]['19'][doc['19']]['strength']*doc['27']['y'])/(info[floor_id]['17'][doc['17']]['strength']*doc['26']['y']))*1.2
-        return min(x0, x1, x2)
+        return min(x0, x1, x2)*info[floor_id]['28'][doc['28']]['strength']
 
     #2b   12-34 Roofcladding-28 Roofarea-31
     if floor_id == "2b":
@@ -262,7 +262,7 @@ def irregulaties(floor_id, doc):
         x2 = ((doc['32']['y']*info[floor_id]['19'][doc['19']]['strength'])/(doc['33']['y']*info[floor_id]['21'][doc['21']]['strength']))*1.2
         x3 = ((info[floor_id]['22'][doc['22']]['strength']*doc['34']['x'])/(doc['32']['y']*info[floor_id]['19'][doc['19']]['strength']))*1.2
         x4 = ((info[floor_id]['23'][doc['23']]['strength']*doc['34']['y'])/(doc['32']['y']*info[floor_id]['19'][doc['19']]['strength']))*1.2
-        return min(x0, x1, x2, x3, x4)
+        return min(x0, x1, x2, x3, x4)*info[floor_id]['35'][doc['35']]['strength']
 
     #3b   12-41 Roofcladding-28 Roofarea-37
     if floor_id == "3b":
@@ -273,7 +273,7 @@ def irregulaties(floor_id, doc):
         x4 = ((info[floor_id]['21'][doc['21']]['strength']*doc['38']['y'])/(info[floor_id]['23'][doc['23']]['strength']*doc['39']['y']))*1.2
         x5 = ((info[floor_id]['26'][doc['26']]['strength'] * doc['41']['x'])/(info[floor_id]['20'][doc['20']]['strength']*doc['38']['x']))*1.2
         x6 = ((info[floor_id]['27'][doc['27']]['strength'] * doc['41']['y'])/(info[floor_id]['21'][doc['21']]['strength']*doc['38']['y']))*1.2
-        return min(x0, x1, x2, x3, x4, x5, x6)
+        return min(x0, x1, x2, x3, x4, x5, x6)*info[floor_id]['42'][doc['42']]['strength']
       
 
     return 'success'
