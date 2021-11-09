1a83eabb913d18bb95acdd1d0e4c26c1bd3183ae
Charlie <cmthomlinson@gmail.com>
2021-11-09 13:00:49 +1300

    commit

--- app.py
+++ app.py
@@ -149,22 +149,22 @@ def clad_struct_average(floor_id, doc):
     info = json.load(f)
 
     if floor_id == "1":
-        clad_av = (checkox_av(floor_id, doc, '17', 'damage') + checkox_av(floor_id, doc, '18', 'damage') + 3)/5
-        structure_av = (checkox_av(floor_id, doc, '8', 'strength') + info[floor_id]['9'][doc['9']]['damage'] + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['15'][doc['15']]['damage'] + info[floor_id]['16'][doc['16']]['damage'] + 12)/8
+        clad_av = (checkox_av(floor_id, doc, '16', 'damage') + checkox_av(floor_id, doc, '17', 'damage') + 3)/5
+        structure_av = (checkox_av(floor_id, doc, '8', 'strength') + info[floor_id]['9'][doc['9']]['damage'] + 1 + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['15'][doc['15']]['damage'] + 12)/8
         print("clad_av: {}".format(clad_av))
         print("structure_av: {}".format(structure_av))
         return clad_av*structure_av
 
     if floor_id == "2":
-        clad_av = (checkox_av(floor_id, doc, '21', 'damage') + checkox_av(floor_id, doc, '22', 'damage') + checkox_av(floor_id, doc, '23', 'damage') + 2)/5
-        structure_av = (checkox_av(floor_id, doc, '8', 'strength') + info[floor_id]['9'][doc['9']]['damage'] + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['15'][doc['15']]['damage'] + info[floor_id]['17'][doc['17']]['damage'] + info[floor_id]['18'][doc['18']]['damage'] + info[floor_id]['19'][doc['19']]['damage'] + info[floor_id]['20'][doc['20']]['damage'] + 9)/18
+        clad_av = (checkox_av(floor_id, doc, '20', 'damage') + checkox_av(floor_id, doc, '21', 'damage') + checkox_av(floor_id, doc, '22', 'damage') + 2)/5
+        structure_av = (checkox_av(floor_id, doc, '8', 'strength') + info[floor_id]['9'][doc['9']]['damage'] + 1 + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['16'][doc['16']]['damage'] + info[floor_id]['17'][doc['17']]['damage'] + info[floor_id]['18'][doc['18']]['damage'] + info[floor_id]['19'][doc['19']]['damage'] + 9)/18
         print("clad_av: {}".format(clad_av))
         print("structure_av: {}".format(structure_av))
         return clad_av*structure_av
 
     if floor_id == "3":
-        clad_av = (checkox_av(floor_id, doc, '25', 'damage') + checkox_av(floor_id, doc, '26', 'damage') + checkox_av(floor_id, doc, '27', 'damage') + checkox_av(floor_id, doc, '28', 'damage') + 1)/5
-        structure_av = (checkox_av(floor_id, doc, '8', 'strength') + info[floor_id]['9'][doc['9']]['damage'] + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['15'][doc['15']]['damage'] + info[floor_id]['16'][doc['16']]['damage'] + info[floor_id]['17'][doc['17']]['damage'] + info[floor_id]['18'][doc['18']]['damage'] + info[floor_id]['19'][doc['19']]['damage'] + info[floor_id]['20'][doc['20']]['damage'] + info[floor_id]['21'][doc['21']]['damage'] + info[floor_id]['22'][doc['22']]['damage'] + info[floor_id]['23'][doc['23']]['damage'] + info[floor_id]['24'][doc['24']]['damage'] + 4)/18
+        clad_av = (checkox_av(floor_id, doc, '24', 'damage') + checkox_av(floor_id, doc, '25', 'damage') + checkox_av(floor_id, doc, '26', 'damage') + checkox_av(floor_id, doc, '27', 'damage') + 1)/5
+        structure_av = (checkox_av(floor_id, doc, '8', 'strength') + info[floor_id]['9'][doc['9']]['damage'] + 1 + info[floor_id]['13'][doc['13']]['damage'] + info[floor_id]['14'][doc['14']]['damage'] + info[floor_id]['15'][doc['15']]['damage'] + info[floor_id]['16'][doc['16']]['damage'] + info[floor_id]['17'][doc['17']]['damage'] + info[floor_id]['18'][doc['18']]['damage'] + info[floor_id]['19'][doc['19']]['damage'] + info[floor_id]['20'][doc['20']]['damage'] + info[floor_id]['21'][doc['21']]['damage'] + info[floor_id]['22'][doc['22']]['damage'] + info[floor_id]['23'][doc['23']]['damage'] + 4)/18
         print("clad_av: {}".format(clad_av))
         print("structure_av: {}".format(structure_av))
         return clad_av*structure_av
@@ -231,23 +231,23 @@ def irregulaties(floor_id, doc):
     info = json.load(f)
     if floor_id == "1":
 
-        return 1*info[floor_id]['22'][doc['22']]['strength']
+        return 1*info[floor_id]['21'][doc['21']]['strength']
 
     #2   12-27 Roofcladding-20 Roofarea-25
     if floor_id == "2":
         x0 = 1
-        x1 = ((doc['27']['x']*info[floor_id]['17'][doc['17']]['strength'])/(doc['28']['x']*info[floor_id]['19'][doc['19']]['strength']))*1.2
-        x2 = ((doc['27']['y']*info[floor_id]['18'][doc['18']]['strength'])/(doc['28']['y']*info[floor_id]['20'][doc['20']]['strength']))*1.2
-        return min(x0, x1, x2)*info[floor_id]['29'][doc['29']]['strength']
+        x1 = ((doc['26']['x']*info[floor_id]['16'][doc['16']]['strength'])/(doc['27']['x']*info[floor_id]['18'][doc['18']]['strength']))*1.2
+        x2 = ((doc['26']['y']*info[floor_id]['17'][doc['17']]['strength'])/(doc['27']['y']*info[floor_id]['19'][doc['19']]['strength']))*1.2
+        return min(x0, x1, x2)*info[floor_id]['28'][doc['28']]['strength']
 
     #3   12-34 Roofcladding-24 Roofarea-31
     if floor_id == "3":
         x0 = 1
-        x1 = ((info[floor_id]['21'][doc['21']]['strength']*doc['34']['x'])/(info[floor_id]['23'][doc['23']]['strength']*doc['35']['x']))*1.2
-        x2 = ((info[floor_id]['22'][doc['22']]['strength']*doc['34']['y'])/(info[floor_id]['24'][doc['24']]['strength']*doc['35']['y']))*1.2
-        x3 = ((info[floor_id]['19'][doc['19']]['strength']*doc['33']['x'])/(info[floor_id]['21'][doc['21']]['strength']*doc['34']['x']))*1.2
-        x4 = ((info[floor_id]['20'][doc['20']]['strength']*doc['33']['y'])/(info[floor_id]['22'][doc['22']]['strength']*doc['34']['y']))*1.2
-        return min(x0, x1, x2, x3, x4)*info[floor_id]['36'][doc['36']]['strength']
+        x1 = ((info[floor_id]['20'][doc['20']]['strength']*doc['33']['x'])/(info[floor_id]['22'][doc['22']]['strength']*doc['34']['x']))*1.2
+        x2 = ((info[floor_id]['21'][doc['21']]['strength']*doc['33']['y'])/(info[floor_id]['23'][doc['23']]['strength']*doc['34']['y']))*1.2
+        x3 = ((info[floor_id]['18'][doc['18']]['strength']*doc['32']['x'])/(info[floor_id]['20'][doc['20']]['strength']*doc['33']['x']))*1.2
+        x4 = ((info[floor_id]['19'][doc['19']]['strength']*doc['32']['y'])/(info[floor_id]['21'][doc['21']]['strength']*doc['33']['y']))*1.2
+        return min(x0, x1, x2, x3, x4)*info[floor_id]['35'][doc['35']]['strength']
     #1b   12-27 Roofcladding-20 Roofarea-25
     if floor_id == "1b":
         x0 = 1
@@ -285,28 +285,28 @@ def floor_area_wall_bracing(floor_id, doc):
 
     #1   12-20 Roofcladding-16 Roofarea-19
     if floor_id == "1":
-        x1 = (50/3)/(((doc['20']['x'] * doc['20']['y'] * checkox_av(floor_id, doc, '17', 'strength')) + (doc['20']['x'] + doc['20']['y'])*2*((checkox_av(floor_id, doc, '18', 'strength') + info[floor_id]['13'][doc['13']]['strength'])/2))/(info[floor_id]['15'][doc['15']]['strength']*doc['21']['x']))
-        x2 = (50/3)/(((doc['20']['x'] * doc['20']['y'] * checkox_av(floor_id, doc, '17', 'strength')) + (doc['20']['x'] + doc['20']['y'])*2*((checkox_av(floor_id, doc, '18', 'strength') + info[floor_id]['13'][doc['13']]['strength'])/2))/(info[floor_id]['16'][doc['16']]['strength']*doc['21']['y']))
+        x1 = (50/3)/(((doc['19']['x'] * doc['19']['y'] * checkox_av(floor_id, doc, '16', 'strength')) + (doc['19']['x'] + doc['19']['y'])*2*((checkox_av(floor_id, doc, '17', 'strength') + 1)/2))/(info[floor_id]['14'][doc['14']]['strength']*doc['20']['x']))
+        x2 = (50/3)/(((doc['19']['x'] * doc['19']['y'] * checkox_av(floor_id, doc, '16', 'strength')) + (doc['19']['x'] + doc['19']['y'])*2*((checkox_av(floor_id, doc, '17', 'strength') + 1)/2))/(info[floor_id]['15'][doc['15']]['strength']*doc['20']['y']))
         print(min(x1, x2))
         return min(x1, x2)
 
     #2   12-27 Roofcladding-20 Roofarea-25
     if floor_id == "2":
-        x1 = (50/3)/((doc['26']['x'] * doc['26']['y'] * checkox_av(floor_id, doc, '21', 'strength') + (doc['25']['x'] + doc['25']['y'])*2*((checkox_av(floor_id, doc, '23', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2))/(doc['28']['x']*info[floor_id]['19'][doc['19']]['strength']))
-        x2 = (50/3)/((doc['26']['x'] * doc['26']['y'] * checkox_av(floor_id, doc, '21', 'strength') + (doc['25']['x'] + doc['25']['y'])*2*((checkox_av(floor_id, doc, '23', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2))/(doc['28']['y']*info[floor_id]['20'][doc['20']]['strength']))
-        x3 = (50/3)/((doc['26']['x'] * doc['26']['y'] * checkox_av(floor_id, doc, '21', 'strength') + (doc['25']['x'] + doc['25']['y'])*2*((checkox_av(floor_id, doc, '23', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2) + (doc['25']['x'] * doc['25']['y'])*info[floor_id]['16'][doc['16']]['strength'] + (doc['24']['x'] + doc['24']['y'])*2*((checkox_av(floor_id, doc, '22', 'strength') + info[floor_id]['13'][doc['13']]['strength'])/2))/(doc['27']['x']*info[floor_id]['17'][doc['17']]['strength']))
-        x4 = (50/3)/((doc['26']['x'] * doc['26']['y'] * checkox_av(floor_id, doc, '21', 'strength') + (doc['25']['x'] + doc['25']['y'])*2*((checkox_av(floor_id, doc, '23', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2) + (doc['25']['x'] * doc['25']['y'])*info[floor_id]['16'][doc['16']]['strength'] + (doc['24']['x'] + doc['24']['y'])*2*((checkox_av(floor_id, doc, '22', 'strength') + info[floor_id]['13'][doc['13']]['strength'])/2))/(doc['27']['y']*info[floor_id]['18'][doc['18']]['strength']))
+        x1 = (50/3)/((doc['25']['x'] * doc['25']['y'] * checkox_av(floor_id, doc, '20', 'strength') + (doc['24']['x'] + doc['24']['y'])*2*((checkox_av(floor_id, doc, '22', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2))/(doc['27']['x']*info[floor_id]['18'][doc['18']]['strength']))
+        x2 = (50/3)/((doc['25']['x'] * doc['25']['y'] * checkox_av(floor_id, doc, '20', 'strength') + (doc['24']['x'] + doc['24']['y'])*2*((checkox_av(floor_id, doc, '22', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2))/(doc['27']['y']*info[floor_id]['19'][doc['19']]['strength']))
+        x3 = (50/3)/((doc['25']['x'] * doc['25']['y'] * checkox_av(floor_id, doc, '20', 'strength') + (doc['24']['x'] + doc['24']['y'])*2*((checkox_av(floor_id, doc, '22', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2) + (doc['24']['x'] * doc['24']['y'])*info[floor_id]['15'][doc['15']]['strength'] + (doc['23']['x'] + doc['23']['y'])*2*((checkox_av(floor_id, doc, '21', 'strength') + 1)/2))/(doc['26']['x']*info[floor_id]['16'][doc['16']]['strength']))
+        x4 = (50/3)/((doc['25']['x'] * doc['25']['y'] * checkox_av(floor_id, doc, '20', 'strength') + (doc['24']['x'] + doc['24']['y'])*2*((checkox_av(floor_id, doc, '22', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2) + (doc['24']['x'] * doc['24']['y'])*info[floor_id]['15'][doc['15']]['strength'] + (doc['23']['x'] + doc['23']['y'])*2*((checkox_av(floor_id, doc, '21', 'strength') + 1)/2))/(doc['26']['y']*info[floor_id]['17'][doc['17']]['strength']))
         print(min(x1, x2, x3, x4))
         return min(x1, x2, x3, x4)
 
     #3   12-34 Roofcladding-24 Roofarea-31
     if floor_id == "3":
-        x1 = (50/3)/(((doc['32']['x'] * doc['32']['y'] * checkox_av(floor_id, doc, '25', 'strength') + (doc['31']['x'] + doc['31']['y'])*2*((checkox_av(floor_id, doc, '28', 'strength') + info[floor_id]['17'][doc['17']]['strength'])/2)))/(info[floor_id]['23'][doc['23']]['strength']*doc['35']['x']))
-        x2 = (50/3)/(((doc['32']['x'] * doc['32']['y'] * checkox_av(floor_id, doc, '25', 'strength') + (doc['31']['x'] + doc['31']['y'])*2*((checkox_av(floor_id, doc, '28', 'strength') + info[floor_id]['17'][doc['17']]['strength'])/2)))/(info[floor_id]['24'][doc['24']]['strength']*doc['35']['y']))
-        x3 = (50/3)/(((doc['32']['x'] * doc['32']['y'] * checkox_av(floor_id, doc, '25', 'strength') + (doc['31']['x'] + doc['31']['y'])*2*((checkox_av(floor_id, doc, '28', 'strength') + info[floor_id]['17'][doc['17']]['strength'])/2) + (doc['31']['x'] * doc['31']['y'])*info[floor_id]['18'][doc['18']]['strength'] + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2))/(info[floor_id]['21'][doc['21']]['strength']*doc['34']['x'])))
-        x4 = (50/3)/(((doc['32']['x'] * doc['32']['y'] * checkox_av(floor_id, doc, '25', 'strength') + (doc['31']['x'] + doc['31']['y'])*2*((checkox_av(floor_id, doc, '28', 'strength') + info[floor_id]['17'][doc['17']]['strength'])/2) + (doc['31']['x'] * doc['31']['y'])*info[floor_id]['18'][doc['18']]['strength'] + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2))/(info[floor_id]['22'][doc['22']]['strength']*doc['34']['y'])))
-        x5 = (50/3)/((doc['32']['x'] * doc['32']['y'] * checkox_av(floor_id, doc, '25', 'strength') + (doc['31']['x'] + doc['31']['y'])*2*((checkox_av(floor_id, doc, '28', 'strength') + info[floor_id]['17'][doc['17']]['strength'])/2) + (doc['31']['x'] * doc['31']['y'])*info[floor_id]['18'][doc['18']]['strength'] + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2) + (doc['30']['x']*doc['30']['y'])*info[floor_id]['16'][doc['16']]['strength'] + (doc['29']['x'] + doc['29']['y'])*2*((checkox_av(floor_id, doc, '26', 'strength') + info[floor_id]['13'][doc['13']]['strength'])/2))/(info[floor_id]['19'][doc['19']]['strength']*doc['33']['x']))
-        x6 = (50/3)/((doc['32']['x'] * doc['32']['y'] * checkox_av(floor_id, doc, '25', 'strength') + (doc['31']['x'] + doc['31']['y'])*2*((checkox_av(floor_id, doc, '28', 'strength') + info[floor_id]['17'][doc['17']]['strength'])/2) + (doc['31']['x'] * doc['31']['y'])*info[floor_id]['18'][doc['18']]['strength'] + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['15'][doc['15']]['strength'])/2) + (doc['30']['x']*doc['30']['y'])*info[floor_id]['16'][doc['16']]['strength'] + (doc['29']['x'] + doc['29']['y'])*2*((checkox_av(floor_id, doc, '26', 'strength') + info[floor_id]['13'][doc['13']]['strength'])/2))/(info[floor_id]['20'][doc['20']]['strength']*doc['33']['y']))
+        x1 = (50/3)/(((doc['31']['x'] * doc['31']['y'] * checkox_av(floor_id, doc, '24', 'strength') + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['16'][doc['16']]['strength'])/2)))/(info[floor_id]['22'][doc['22']]['strength']*doc['34']['x']))
+        x2 = (50/3)/(((doc['31']['x'] * doc['31']['y'] * checkox_av(floor_id, doc, '24', 'strength') + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['16'][doc['16']]['strength'])/2)))/(info[floor_id]['23'][doc['23']]['strength']*doc['34']['y']))
+        x3 = (50/3)/(((doc['31']['x'] * doc['31']['y'] * checkox_av(floor_id, doc, '24', 'strength') + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['16'][doc['16']]['strength'])/2) + (doc['30']['x'] * doc['30']['y'])*info[floor_id]['17'][doc['17']]['strength'] + (doc['29']['x'] + doc['29']['y'])*2*((checkox_av(floor_id, doc, '26', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2))/(info[floor_id]['20'][doc['20']]['strength']*doc['33']['x'])))
+        x4 = (50/3)/(((doc['31']['x'] * doc['31']['y'] * checkox_av(floor_id, doc, '24', 'strength') + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['16'][doc['16']]['strength'])/2) + (doc['30']['x'] * doc['30']['y'])*info[floor_id]['17'][doc['17']]['strength'] + (doc['29']['x'] + doc['29']['y'])*2*((checkox_av(floor_id, doc, '26', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2))/(info[floor_id]['21'][doc['21']]['strength']*doc['33']['y'])))
+        x5 = (50/3)/((doc['31']['x'] * doc['31']['y'] * checkox_av(floor_id, doc, '24', 'strength') + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['16'][doc['16']]['strength'])/2) + (doc['30']['x'] * doc['30']['y'])*info[floor_id]['17'][doc['17']]['strength'] + (doc['29']['x'] + doc['29']['y'])*2*((checkox_av(floor_id, doc, '26', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2) + (doc['29']['x']*doc['29']['y'])*info[floor_id]['15'][doc['15']]['strength'] + (doc['28']['x'] + doc['28']['y'])*2*((checkox_av(floor_id, doc, '25', 'strength') + 1)/2))/(info[floor_id]['18'][doc['18']]['strength']*doc['32']['x']))
+        x6 = (50/3)/((doc['31']['x'] * doc['31']['y'] * checkox_av(floor_id, doc, '24', 'strength') + (doc['30']['x'] + doc['30']['y'])*2*((checkox_av(floor_id, doc, '27', 'strength') + info[floor_id]['16'][doc['16']]['strength'])/2) + (doc['30']['x'] * doc['30']['y'])*info[floor_id]['17'][doc['17']]['strength'] + (doc['29']['x'] + doc['29']['y'])*2*((checkox_av(floor_id, doc, '26', 'strength') + info[floor_id]['14'][doc['14']]['strength'])/2) + (doc['29']['x']*doc['29']['y'])*info[floor_id]['15'][doc['15']]['strength'] + (doc['28']['x'] + doc['28']['y'])*2*((checkox_av(floor_id, doc, '25', 'strength') + 1)/2))/(info[floor_id]['19'][doc['19']]['strength']*doc['32']['y']))
         print(min(x1, x2, x3, x4, x5, x6))
         return min(x1, x2, x3, x4, x5, x6)
 
