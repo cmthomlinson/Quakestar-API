4a577dc9cb5afcf8495cb577f42ec80550e9ebfb
Charlie <cmthomlinson@gmail.com>
2021-12-20 19:56:47 +1300

    commit

--- calculations/damage.py
+++ calculations/damage.py
@@ -8,13 +8,12 @@ def damage_all(floor_id, doc):
     info = json.load(f)
     site = max(info[floor_id]['2'][doc['2']]['damage'], info[floor_id]['3'][doc['3']]['damage'], 1, info[floor_id]['3'][doc['3']]['damage'])
     building = (info[floor_id]['5'][doc['5']]['damage'] * info[floor_id]['6'][doc['6']]['damage'] * info[floor_id]['7'][doc['7']]['damage'])
-    appendages = max(irregulaties(floor_id, doc), max(info[floor_id]['10'][doc['10']]['strength'], info[floor_id]['11'][doc['11']]['strength'], info[floor_id]['12'][doc['12']]['strength']))
     print("site_damage: {}".format(site))
     print("building_damage: {}".format(building))
 
     
 
-    return site*building*clad_struct_average(floor_id, doc)*appendages
+    return site*building*clad_struct_average(floor_id, doc)
 
 
 def clad_struct_average(floor_id, doc): 
